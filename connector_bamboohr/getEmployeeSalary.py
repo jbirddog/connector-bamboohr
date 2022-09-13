7ed7c3820e705bc8f3a39bf7702f9111dc5dce34
Jon Herron <jon.herron@yahoo.com>
2022-09-13 11:56:57 -0400

    Skeleton

---
+++ connector_bamboohr/getEmployeeSalary.py
@@ -0,0 +1,7 @@
+class GetEmployeeSalary:
+    def __init__(self, api_key: str, employee_id: str):
+        self.api_key = api_key
+        self.employee_id = employee_id
+
+    def execute():
+        return '{"todo": "get employee salary" };
